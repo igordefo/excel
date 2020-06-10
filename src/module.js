62c2c57d86a5114bdcf0095b4361f75384df51b0
igordefo <dekleidung@gmail.com>
2020-06-10 20:24:10 +0300

    feat: add configuration for webpack loaders

--- src/module.js
+++ src/module.js
@@ -1,1 +1,3 @@
 console.log("Module works!");
+
+[1, 2, 3].map((n) => n + 1);
--- webpack.config.js
+++ webpack.config.js
@@ -34,5 +34,27 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'bundle.[hash].css'
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ],
+  },
 }
